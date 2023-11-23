d19d159ffaf8588a38e1731c2302d6e2200b4df3
Riza Mohamed <90960705+CODEWITHRIZA@users.noreply.github.com>
2023-11-23 14:14:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import openai
+
+openai.api_key = "YOUR_OPENAI_API_KEY"
+
+def generate_story(topic):
+    prompt_text = f"Once upon a time, there was a {topic}. "
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt_text,
+        max_tokens=300
+    )
+    return response.choices[0].text.strip()
+
+def main():
+    st.title("SummariGenius")
+
+    api_key = st.text_input("Enter your OpenAI API key", type="password")
+    topic = st.text_input("Enter a topic")
+
+    if st.button("Generate Story"):
+        if api_key and topic:
+            openai.api_key = api_key
+            st.write("Generating story...")
+            story = generate_story(topic)
+            st.write("### Generated Story")
+            st.write(story)
+        elif not api_key:
+            st.warning("Please enter your OpenAI API key.")
+        else:
+            st.warning("Please enter a topic.")
+
+    st.markdown("---")
+    st.write("Made by Riza Mohamed T ")
+    st.write("[GitHub Profile](https://github.com/codewithriza)")
+    st.write("[Portfolio Website](https://rizamohamed.netlify.app)")
+    st.write("[Mail](mailto:codewithriza@gmail.com)")
+
+if __name__ == "__main__":
+    main()
